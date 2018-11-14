f9fa176558a78ec33d554e98e6a37a79d72be368
Abdullah Siddiqi <abdullah.siddiqi2445@gmail.com>
2018-11-13 22:30:49 -0500

    Finished Tags

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -4,7 +4,7 @@
 
 module ArticlesHelper
     def article_params
-        params.require(:article).permit(:title, :body)
+        params.require(:article).permit(:title, :body, :tag_list)
     end
       
 end
